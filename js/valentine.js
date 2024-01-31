675cae426dfd1c7e4e7dde6fb62aa60aaeffca1f
Gloubiboulgaah <gloubiboulga.sh@gmail.com>
2024-01-31 14:35:34 +0100

    valentine

---
+++ js/valentine.js
@@ -0,0 +1,33 @@
+var divYes = document.getElementById('yes');
+var divNon = document.getElementById('no');
+var divTitle = document.getElementById('title');
+var divSubTitle = document.getElementById('subtitle')
+var body = document.getElementById('body');
+var image = document.getElementById('image');
+
+divYes.addEventListener('mouseover', function(e) {
+    document.addEventListener('mousemove', function(e) {
+    divYes.style.left = (e.pageX - divYes.offsetWidth / 2) + 'px';
+    divYes.style.top = (e.pageY - divYes.offsetHeight / 2) + 'px';
+    });
+});
+
+divYes.addEventListener('click', function(e) {
+    divTitle.textContent = "Parfait ^^"
+    divSubTitle.textContent = "On se retrouve le 14 février ^^"
+    image.src = "../images/amour.gif"
+})
+
+divNon.addEventListener('click', function() {
+    var audio = new Audio('../audio/criminal.mp3');
+    audio.loop = true;
+    audio.play();
+    divTitle.textContent = "crève.";
+
+    body.style.backgroundColor = "black";
+    divTitle.style.color = "white";
+    divTitle.style.fontStyle = "italic";
+    divTitle.style.fontWeight = "normal"
+    divSubTitle.style.visibility = "hidden";
+    divYes.style.visibility = "hidden";
+});
